fe3759e43c7e1582bca3f0cad13b84b3c57fe8a2
vanekarenina <90297907+vanekarenina@users.noreply.github.com>
2022-05-23 21:28:09 +0700

    Update Login.py

--- Login.py
+++ Login.py
@@ -4,17 +4,18 @@ import tkinter.messagebox as msg
 from tkinter import ttk
 import datetime as dt
 
+#Membuat sebuah class untuk methodnya
 class Loginregisteruser:
     def main_screen(self):
         Button(text='Login',bg= "White",fg='Black',height= '2',width= '30',command = self.login).place(x=140, y=160)
-    
+    #inisialisasi
     def __init__(self,gui,header):
         self.gui = gui
         self.gui.geometry('480x318')
         self.gui.title(header)
         self.gui.resizable(0,0)
         self.main_screen()
-
+    #menu login
     def login(self):
         screen1 = Toplevel(app)
         screen1.title("Login CGV CINEMA")
@@ -30,7 +31,7 @@ class Loginregisteruser:
         self.showPass = Checkbutton(screen1, text = "Show Password", variable=self.check,command=self.open_password).pack(expand=False,fill=BOTH,padx=10,pady=5)
         self.showPass
         self.btnlogin = Button(screen1, text="Login", bg= 'white',fg='black',command=self.do_login).pack(side=LEFT,expand=True,fill= BOTH, padx= 10,pady=5)
-        
+    #Untuk mengecek input yang dimasukan, apakah sudah sesuai dengan file database
     def do_login(self):
         get_username = self.entryUser.get()
         get_password = self.entryPass.get()
@@ -53,7 +54,8 @@ class Loginregisteruser:
         else :
             msg.showerror('Login Gagal','Email atau Password Salah',parent=self.gui)
             self.delete_data()
-    
+
+    #menampilkan password
     def open_password(self):
         Show = self.check.get()      
         if Show == 1:
@@ -61,19 +63,20 @@ class Loginregisteruser:
         else :
             self.entryPass['show']='#'
 
+    #Menampilkan menu pesan tiket
     def sec_screen(self):
         global screen2
         screen2 = Toplevel(app)
         screen2.title("CGV CINEMA")
         screen2.geometry('400x450')
         screen2.resizable(0,0)
-              
+        #Header pesan tiket
         Label(screen2,text="PESAN TIKET" ,bg='Black',width='300',height='2',font= ('courier new',13, "bold"),fg='white').pack()
-        Label(screen2, text='').pack()
+        Label(screen2,text='').pack()
         Label(screen2,text= "Nama\t\t:",).place(x=30, y=60)
         self.entryID = Entry(screen2, font = ("times new roman", 10))
         self.entryID.place(x=140, y=57)
-
+        #button untuk kursi
         self.radiochr = IntVar()
         Label(screen2, text = "Seet\t\t:").place(x=30, y=80)
         self.entrykursi1 = Radiobutton(screen2, text = 'A', variable=self.radiochr,value =1,command=self.seet)
@@ -83,7 +86,7 @@ class Loginregisteruser:
         self.entrykursi3 = Radiobutton(screen2, text = 'C', variable=self.radiochr,value =3,command=self.seet2)
         self.entrykursi3.place(x = 140, y = 120)
         Label(screen2, text='Nomor\t\t:').place(x=30, y=140)
-        
+        #button untuk memilih film
         self.radio = IntVar()
         Label(screen2, text = "Genre\t\t:").place(x=30, y=165)
         self.entrySrv1 = Radiobutton(screen2, text = 'Romance', variable=self.radio,value =1,command=self.roman_srv)
@@ -92,28 +95,28 @@ class Loginregisteruser:
         self.entrySrv2.place(x = 140, y = 185)
         self.entrySrv3 = Radiobutton(screen2, text = 'Horor', variable=self.radio,value =3,command=self.hor_srv)
         self.entrySrv3.place(x = 140, y = 205)
-        
         Label(screen2, text='Film\t\t:').place(x=30, y=230)
+        #button untuk memilih harga
         Label (screen2, text ="Price List\t\t:").place(x=30, y=260)
         self.radiobtn = IntVar()
-        self.radio1 = Radiobutton(screen2, text="Reguler - 30K",variable = self.radiobtn,value=31000).place(x = 140, y = 260)
-        self.radio2 = Radiobutton(screen2,text="Sweet Box - 60K",variable = self.radiobtn,value=63000).place(x = 140, y = 280)
-        
+        self.radio1 = Radiobutton(screen2, text="Reguler - 30K",variable = self.radiobtn,value=30000).place(x = 140, y = 260)
+        self.radio2 = Radiobutton(screen2,text="Sweet Box - 60K",variable = self.radiobtn,value=60000).place(x = 140, y = 280)
+        #button untuk memilih payment
         Label(screen2,text="Payment Method\t:").place(x=30, y=320)
         self.strpym = StringVar(value='...') 
         self.combobox1 = ttk.Combobox(screen2,width = 17,font = ("times new roman", 10), textvariable = self.strpym, state ="readonly")
         self.combobox1.place(x=140, y=320)
-        self.combobox1['values'] = ('Gopay','Dana','OVO','BCA','BRI')
-        
+        self.combobox1['values'] = ('Gopay','Dana','OVO','BCA','BRI','BNI')
         btn = Button(screen2, text="Pay", command=self.payment,bg= 'lightgreen',state=ACTIVE)
         btn.place(x=270 ,y=317)
-        
+        #buat menekan order
         self.btnsub = Button(screen2, text="Order",font=('helvetica',13,'bold'),bg = "green",fg = "white",command = self.btn_sub,state = ACTIVE)
         self.btnsub.place(x = 320, y = 380)
 
+    #Buat menentukan payment, agar sesuai dengan data yang diinginkan
+    #Misalnya e-wallet akan menampilkan no hp
     def payment(self):
         pym = self.strpym.get()
-        
         if pym == 'Gopay' :
             Label(screen2, text='No Handphone\t\t:').place(x=30,y=347)
             self.entrynum = Entry(screen2, font = ("times new roman", 10))
@@ -135,18 +138,18 @@ class Loginregisteruser:
             self.entryrek = Entry(screen2, font=("times new roman", 10))
             self.entryrek.place(x=140, y=347)
 
+    #Mengambil data yang dimasukan, dan menampilkannya
     def btn_sub(self):
         name = self.entryID.get()
         kursi = self.radiochr.get()
-        srv = self.radio.get()
+        gnr = self.radio.get()
         nom = self.radiobtn.get()
         pym = self.strpym.get()
         date = dt.datetime.now()
-        
         if name == "" :
             msg.showwarning("Peringatan","Nama Tidak Boleh Kosong!",parent=self.gui)
             return
-        if srv == 0:
+        if gnr == 0:
             msg.showwarning("Peringatan","Genre Tidak Boleh Kosong!",parent=self.gui)
             return
         if nom == 0:
@@ -188,6 +191,7 @@ class Loginregisteruser:
         self.chair1 = ttk.Combobox(screen2, width=15,font=('times new romance',9),textvariable=self.chair0,state="readonly")
         self.chair1.place(x=140, y=140)
         self.chair1 ['values'] = ('1','2','3','4')
+
     def seet2(self):
         self.chair0 = StringVar(value="...")
         self.chair1 = ttk.Combobox(screen2,width=15,font=('times new romance',9),textvariable=self.chair0,state="readonly")
@@ -220,6 +224,7 @@ class Loginregisteruser:
     def close_gui(self):
         self.gui.destroy()
 
+#Main menu agar GUI dapat berjalan
 if __name__ == '__main__':
     global app
     app = Tk()
